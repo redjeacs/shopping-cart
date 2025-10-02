70fc116a51f7063efc12067cf935f37ca0dbdc80
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-10-02 16:21:39 +0800

    home component

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,5 +1,19 @@
+import styles from "./home.module.css";
+import { Link } from "react-router";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <section>
+      <img src="../src/assets/home-image.jpg" alt="" />
+      <div className={styles.shopRedirect}>
+        <h2>Welcome to SweatShop!</h2>
+        <p>Your one stop for all your clothing needs</p>
+        <Link className={styles.shopLink} to="shop">
+          Shop Now
+        </Link>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
