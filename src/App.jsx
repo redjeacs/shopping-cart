76f610aaa738d5e9d701c0c542eaa72c45c17bfc
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-10-02 14:30:54 +0800

    nav styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 import Nav from "./components/nav/nav";
-import routes from "./routes";
 
 function App() {
   return (
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -5,10 +5,13 @@ import { House, ShoppingCart } from "lucide-react";
 function Nav() {
   return (
     <header>
+      <div className={styles.centeringDiv}></div>
       <div className={styles.left}>
-        <div className={styles.logo}>Logo</div>
+        <div className={styles.logo}>SweatShop</div>
         <div className={styles.shopLinks}>
-          <Link to={styles.shop}>Shop</Link>
+          <Link to="shop">Men</Link>
+          <Link to="shop">Women</Link>
+          <Link to="shop">Children</Link>
         </div>
       </div>
       <div className={styles.right}>
