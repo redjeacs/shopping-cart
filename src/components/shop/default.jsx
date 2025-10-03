e398e07bb7216f2c043ab0ee70b1d68fcb0ff050
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2025-10-03 17:46:08 +0800

    default shop page

--- src/components/shop/default.jsx
+++ src/components/shop/default.jsx
@@ -1,5 +1,46 @@
+import { Link } from "react-router";
+import styles from "./default.module.css";
+
 function DefaultShop() {
-  return <div>default</div>;
+  return (
+    <div className={styles.shopContainer}>
+      <div className={styles.discountContainer}>
+        <h2>VIP Sale: extra 30% off</h2>
+        <p>
+          Savings on regular & already-reduced prices. Extra 10-30% off children
+          products.
+        </p>
+      </div>
+      <div className={styles.latestContainer}>
+        <div className={styles.left}>
+          <p>summer apparels</p>
+        </div>
+        <div className={styles.main}>
+          <h2>Fresh looks for your wardrobe</h2>
+        </div>
+        <div className={styles.right}>
+          <p>breathable fabric</p>
+        </div>
+      </div>
+      <div className={styles.categoriesContainer}>
+        <h2>Featured Categories</h2>
+        <div className={styles.links}>
+          <Link to="men" className={styles.link}>
+            <div className={styles.menImage}></div>
+            <p>Men</p>
+          </Link>
+          <Link to="women" className={styles.link}>
+            <div className={styles.womenImage}></div>
+            <p>Women</p>
+          </Link>
+          <Link to="children" className={styles.link}>
+            <div className={styles.childrenImage}></div>
+            <p>Kids</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DefaultShop;
