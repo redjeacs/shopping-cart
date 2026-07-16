4ed80a69ae4ac3b8083b10b299eb60823908cce7
Jerry Chang <213577976+redjeacs@users.noreply.github.com>
2026-07-16 15:57:20 +0800

    add basename to browser router for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import routes from "./routes.jsx";
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, { basename: "/shopping-cart" });
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
